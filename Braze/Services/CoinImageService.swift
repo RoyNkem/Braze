4188171eaa6936539ab9913087139f7e3be39c9e
Roy <95645767+RoyNkem@users.noreply.github.com>
2022-10-07 05:54:21 +0100

    Create an image  download service using the reusable networking layer created before

---
+++ Braze/Services/CoinImageService.swift
@@ -0,0 +1,38 @@
+//
+//  CoinImageService.swift
+//  Braze
+//
+//  Created by Roy Aiyetin on 06/10/2022.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class CoinImageService {
+    
+    @Published var image: UIImage? = nil
+    
+    var imageSubscription: AnyCancellable?
+    private let coin: CoinModel
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        getCoinImage()
+    }
+    
+    private func getCoinImage() {
+        print("downloading images now")
+        guard let url = URL(string: coin.image) else { return }
+        
+        imageSubscription = NetworkingManager.download(url: url)
+            .tryMap({ (data) -> UIImage? in
+                return UIImage(data: data)
+            })
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
+                self?.image = returnedImage
+                self?.imageSubscription?.cancel()
+            })
+    }
+    
+}
